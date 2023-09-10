14009da36105865d1229efaa930adb21223ab951
Adellar Irankunda <addyirankunda@gmail.com>
2023-09-10 00:36:24 -0700

    added sky segmentation scene

---
+++ Assets/SemanticSky.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Niantic.ARDK;
+using Niantic.ARDK.AR;
+using Niantic.Lightship.AR.Extensions;
+using Niantic.Lightship.AR.ARFoundation;
+
+public class SemanticSky : MonoBehaviour
+{
+    public ARSemanticSegmentationManager _segManager;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        _segManager.SemanticModelIsReady += onSegementationReady;
+    }
+
+    void onSegementationReady(ARSemanticModelReadyEventArgs args)
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        _segManager.SemanticModelIsReady -= onSegementationReady;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
